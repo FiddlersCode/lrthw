6878240241e46318fe881c294af8f4039ca7b1a2
Paula <pmuldoon@umich.edu>
2017-03-20 10:17:49 +0000

    3rd Commit

---
+++ ex03/ex03.rb
@@ -0,0 +1,45 @@
+#writes the text to the screen
+puts "I will now count my chickens:"
+
+# calculates the result of the maths in PEMDAS order and puts to screen after string
+puts "Hens #{25 + 30 / 6}"
+puts "Roosters #{100 - 25 * 3 % 4}"
+
+#puts the string
+puts "Now I will count the eggs:"
+
+#calculates the math using PEMDAS and rounds up (6.75 becomes 7)
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+# puts this line
+puts "Is it true that 3 + 2 < 5 - 7?"
+
+#calculates the math and does a true/false statement
+puts 3 + 2 < 5 - 7
+
+#calculates the maths and gives and answer
+puts "What is 3 + 2? #{3 + 2}"
+puts "What is 5 - 7? #{5 - 7}"
+
+#puts string
+puts "Oh, that's why it's false."
+
+#puts string
+puts "How about some more."
+
+# puts string and then calculates the math with but displays a true/false statement, not a numeral
+puts "Is it greater? #{5 > -2}"
+puts "Is it greater or equal? #{5 >= -2}"
+puts "Is it less or equal? #{5 <= -2}"
+
+=begin
++ plus
+- minus
+/ slash
+* asterisk
+ % percent
+ < less-than
+ > greater-than
+ <= less-than-equal
+ >= greater-than-equal
+=end
