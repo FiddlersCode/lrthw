ea6d829ab4a77408c25b1f605c828d37241bf114
Paula <pmuldoon@umich.edu>
2017-03-20 14:15:09 +0000

    13th Commit

---
+++ ex13/ex01.rb
@@ -0,0 +1,17 @@
+first, second, third = ARGV
+
+puts "Your first variable is: #{first}"
+puts "Your second variable is: #{second}"
+puts "Your third variable is: #{third}"
+
+first, second = ARGV
+
+puts "Your first variable is: #{first}"
+puts "Your second variable is: #{second}"
+
+first, second, third, fourth = ARGV
+
+puts "Your first variable is: #{first}"
+puts "Your second variable is: #{second}"
+puts "Your third variable is: #{third}"
+puts "Your fourth variable is: #{fourth}"
