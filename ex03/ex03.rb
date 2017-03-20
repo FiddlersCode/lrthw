4f3e30a631c66923c104cab19e9d18fbc54da60b
Paula <pmuldoon@umich.edu>
2017-03-20 10:27:06 +0000

    3b Commit

--- ex03/ex03.rb
+++ ex03/ex03.rb
@@ -8,18 +8,18 @@ puts "Roosters #{100 - 25 * 3 % 4}"
 #puts the string
 puts "Now I will count the eggs:"
 
-#calculates the math using PEMDAS and rounds up (6.75 becomes 7)
-puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+#calculates the math using PEMDAS and gives a floating point answer
+puts 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
 
-# puts this line
+# puts this line.  Omitted floating points as it's a string, not a calculation
 puts "Is it true that 3 + 2 < 5 - 7?"
 
 #calculates the math and does a true/false statement
-puts 3 + 2 < 5 - 7
+puts 3.0 + 2.0 < 5.0 - 7.0
 
-#calculates the maths and gives and answer
-puts "What is 3 + 2? #{3 + 2}"
-puts "What is 5 - 7? #{5 - 7}"
+#calculates the maths and gives and answer with a floating point.  Omitted floating point in the string.
+puts "What is 3 + 2? #{3.0 + 2.0}"
+puts "What is 5 - 7? #{5.0 - 7.0}"
 
 #puts string
 puts "Oh, that's why it's false."
@@ -27,10 +27,10 @@ puts "Oh, that's why it's false."
 #puts string
 puts "How about some more."
 
-# puts string and then calculates the math with but displays a true/false statement, not a numeral
-puts "Is it greater? #{5 > -2}"
-puts "Is it greater or equal? #{5 >= -2}"
-puts "Is it less or equal? #{5 <= -2}"
+# puts string and then calculates the math with but displays a true/false statement, not a numeral.  NB floating point makes no difference to the answer.
+puts "Is it greater? #{5.0 > -2.0}"
+puts "Is it greater or equal? #{5.0 >= -2.0}"
+puts "Is it less or equal? #{5.0 <= -2.0}"
 
 =begin
 + plus
