348cea42329dfdaf10d15a5bc5970e063791ffb8
Paula <pmuldoon@umich.edu>
2017-03-20 11:51:05 +0000

    6th Commit

---
+++ ex06/ex01.rb
@@ -0,0 +1,31 @@
+# sets variable to 10
+types_of_people = 10
+# sets variable to equal the string below
+x = "There are #{types_of_people} types of people."
+# sets variable
+binary = "binary"
+do_not = "don't"
+# sets variable to equal the string below
+y = "Those who know #{binary} and those who #{do_not}."
+
+# puts those two strings
+puts x
+puts y
+
+#puts the strings with the x and y variables.  NB there are too many periods in line 17.
+puts "I said: #{x}."
+puts "I also said: '#{y}'."
+
+# sets variable hilarious to be false (this is a true/false statement, not "false" a string)
+hilarious = false
+#sets variable to string with hilarious true/false variable
+joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
+
+# puts the joke to the
+puts joke_evaluation
+
+# sets variables
+w = "This s the left side of..."
+e = "a string with a right side."
+
+puts w + e
