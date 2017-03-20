592a3b54c22aa944c6cef3acb403655b1f015a8b
Paula <pmuldoon@umich.edu>
2017-03-20 10:23:40 +0000

    3a Commit

---
+++ ex03/ex02.rb
@@ -0,0 +1,11 @@
+# How many hours of programming can I do today?
+
+puts "I have to leave for Richard's at 16:45."
+
+puts "It's currently 10:20."
+
+puts "So I have #{16.45 - 10.20} hours to code."
+
+puts "I'll be back from Richard's at 17:15 and I have to leave for Rachel's at 19:15."
+
+puts "Then I'll have another #{19.15 - 17.15} hours to code."
