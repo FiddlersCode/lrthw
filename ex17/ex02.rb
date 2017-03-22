49769f38073766db541252bf96350286c5a0d236
Paula <pmuldoon@umich.edu>
2017-03-22 15:28:42 +0000

    17c Commit

--- ex17/ex02.rb
+++ ex17/ex02.rb
@@ -1,15 +1,10 @@
 from_file, to_file = ARGV
 
+# we could do these two on online, how?
+in_file = open(from_file, 'r'); indata = in_file.read
+
+out_file = open(to_file, 'w'); out_file.write(indata)
+
 puts "Copying from #{from_file} to #{to_file}"
 
-# we could do these two on online, how?
-in_file = open(from_file)
-indata = in_file.read
-
-out_file = open(to_file, 'w')
-out_file.write(indata)
-
-puts "Alright, all done."
-
-out_file.close
-in_file.close
+out_file.close; in_file.close
