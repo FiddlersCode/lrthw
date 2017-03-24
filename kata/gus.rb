e7f8affd234d1e10d134b3a93ac0af829bf2cf22
Paula <pmuldoon@umich.edu>
2017-03-24 14:33:59 +0000

    39th Commit

---
+++ kata/gus.rb
@@ -0,0 +1,12 @@
+
+def iterate_array(a)
+  for i in 0..2
+      for j in 0..2
+          puts "Now i value is #{i}"
+      end
+    end
+
+end
+a = [[1,2,3], [4,5,6], [7,8,9]]
+
+iterate_array(a)
--- kata/kata6.rb
+++ kata/kata6.rb
@@ -1,5 +1,6 @@
+
 def same(arr_a, arr_b)
-  arr_a.flatten.sort == arr_b.flatten.sort
+    arr_a.map{ |i| i.sort }.sort == arr_b.map { |i| i.sort }.sort
 end
 
 puts same([[2,5,], [3, 6]], [[2,5], [6,3]])
