0fe2c3657746ab9b98a3b3fc3085c41e7a50020a
Paula <pmuldoon@umich.edu>
2017-03-25 15:23:41 +0000

    40th Commit

---
+++ ex40/class.rb
@@ -0,0 +1,11 @@
+class MyStuff
+
+  def initialize()
+    @tangerine = "And now a thousand years between."
+  end
+
+  attr_reader :TANGERINE
+  def apple()
+    puts "I AM CLASSY APPLES!"
+  end
+end
