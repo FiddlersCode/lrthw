8b44e071a0debfd36f034c345d8e77a3343c1b2f
Paula <pmuldoon@umich.edu>
2017-03-29 14:06:29 +0100

    camel case halfway

--- kata/kata12.rb
+++ kata/kata12.rb
@@ -1,11 +1,11 @@
-def find_it(seq)
-  even_array = []
-  odd_array = []
-  a.each do |a, b|
-      if a.even?
-          even_array << a
-      else
-          odd_array << b
-      end
-    end
+def to_camel_case(str)
+  if str =~ /-/
+    arr = str.split('-')
+    arr.map(&:capitalize).join
+  else str =~ /_/
+    arr = str.split('_')
+    arr.map(&:capitalize).join
+  end
 end
+puts to_camel_case("the-stealth-warrior")
+puts to_camel_case("The_Stealth_Warrior")
